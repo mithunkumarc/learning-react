8eebac4062bf0bc53a44eb4e56ec1e22b421e88b
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-11-05 22:05:57 +0530

    Update App.js

--- hooks/useEffect/useEffect-racecondition-axios/src/App.js
+++ hooks/useEffect/useEffect-racecondition-axios/src/App.js
@@ -5,28 +5,29 @@ import axios from 'axios';
 export default function App() {
   /* initialize todos with empty array if you are exptecting array from api call*/
   const [todos, setTodos] = useState([]);
-  const [lock, setLock] = useState(false);
+  
   useEffect(() => {
-    const fetchData = async () => {
       /* 
         like synchronising in java.
         put lock when request is in process.
         when request is complete, remove lock.
       */
-      if (!lock) {
-        setLock(true);
+    let isActive = true;
+    const fetchData = async () => {
         /* 
           this example may not run into racecondition
-          becuase, url/reuest not dependending on any props/state.
+          because, url/reuest not dependending on any props/state.
           if request depends on props/state, it may run into race condition.  
         */
         const result = await axios('https://jsonplaceholder.typicode.com/todos'); 
-        console.log(result.data);
-        setTodos(result.data);
-        setLock(false);
-      }
+        if(isActive) {
+          setTodos(result.data);
+        }
     };
     fetchData();
+    return () => {
+      isActive = false;
+    }
   },[]); // runs only once when component is mounted/created
   return (
     <div>
